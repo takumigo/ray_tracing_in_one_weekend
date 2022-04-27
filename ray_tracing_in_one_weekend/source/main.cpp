ba07382b28fdd6240330a18ccc9ccaff8f53c69f
takumigo <kotatrou777@gmail.com>
2022-04-27 20:47:28 +0900

    色関係ユーティリティ関数を用い、メインループ書き換え

--- ray_tracing_in_one_weekend/source/main.cpp
+++ ray_tracing_in_one_weekend/source/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "color.h"
+#include "vec3.h"
 
 int main()
 {
@@ -12,15 +14,10 @@ int main()
 		std::cerr << "\nScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto r = double(i) / static_cast<double>((image_width - 1));
-			auto g = double(j) / static_cast<double>((image_height - 1));
-			auto b = 0.25;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			color pixel_color(double(i) / static_cast<double>((image_width - 1)),
+							  double(j) / static_cast<double>((image_height - 1)),
+						      0.25);
+			write_color(std::cout, pixel_color);
 		}
 	}
 	std::cerr << "\nDone.\n";
