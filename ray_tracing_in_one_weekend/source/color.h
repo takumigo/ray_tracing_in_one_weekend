83cc93f21c5328c0d437b0393ce29d72638746d6
takumigo <kotatrou777@gmail.com>
2022-05-08 13:39:33 +0900

    マルチサンプルを使ったレンダリング

--- ray_tracing_in_one_weekend/source/color.h
+++ ray_tracing_in_one_weekend/source/color.h
@@ -2,6 +2,7 @@
 #define COLOR_H
 
 #include "vec3.h"
+#include "rtweekend.h"
 
 #include <iostream>
 
@@ -10,6 +11,24 @@ void write_color(std::ostream& out, color pixel_color)
 	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
 		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
 		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+void write_color(std::ostream& out, color pixel_color, int sample_per_pixel)
+{
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	// 色の和をサンプル数で割る
+	auto scale = 1.0 / sample_per_pixel;
+	r *= scale;
+	g *= scale;
+	b *= scale;
+
+	// 各成分を[0,255]に変換して出力する
+	out << static_cast<int>(255.999 * clamp(r,0.0,0.999)) << ' '
+		<< static_cast<int>(255.999 * clamp(g,0.0,0.999)) << ' '
+		<< static_cast<int>(255.999 * clamp(b,0.0,0.999)) << '\n';
 }
 
 #endif // !COLOR_H
