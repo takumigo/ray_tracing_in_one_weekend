622c13f80d139ede985f0cde0533048e6c8fcb29
takumigo <kotatrou777@gmail.com>
2022-05-18 23:08:55 +0900

    焦点ぼけ

--- ray_tracing_in_one_weekend/source/main.cpp
+++ ray_tracing_in_one_weekend/source/main.cpp
@@ -71,8 +71,13 @@ int main()
 
 	//world.add(make_shared<sphere>(point3(-R,      0, -1), R, make_shared<lambertian>(color(0,0,1))));
 	//world.add(make_shared<sphere>(point3( R,      0, -1), R, make_shared<lambertian>(color(1,0,0))));
+	point3 lookfrom(3, 3, 2);
+	point3 lookat(0, 0, -1);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = (lookfrom - lookat).length();
+	auto aperture = 2.0;
 
-	camera cam(point3(-2,2,1),point3(0,0,-1),vec3(0,1,0),20,aspect_ratio);
+	camera cam(lookfrom,lookat,vup,20,aspect_ratio,aperture,dist_to_focus);
 
 	for (int j = image_height - 1; j >= 0; --j)
 	{
